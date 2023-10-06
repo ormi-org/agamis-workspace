461392bef9727c6111baaa1a926166898e30a9da
p103304 <ethan.broyard@univ-orleans.Fr>
2023-10-06 08:57:16 +0200

    feat[5]: Ajout debut navbar

--- apps/workspace-ui/src/app/app.module.ts
+++ apps/workspace-ui/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavBarComponent } from './components/navBar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    NavBarComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
