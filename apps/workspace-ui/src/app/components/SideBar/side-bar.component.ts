b337830ca8ac49a8aaf97fdca1f3f83500f1949b
brice.morel <brice.morel1@etu.univ-orleans.fr>
2023-11-23 17:01:24 +0100

    affichage side-bar principale

--- apps/workspace-ui/src/app/components/SideBar/side-bar.component.ts
+++ apps/workspace-ui/src/app/components/SideBar/side-bar.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css'],
+  styleUrls: ['./side-bar.component.sass'],
 })
 export class SideBarComponent {}
