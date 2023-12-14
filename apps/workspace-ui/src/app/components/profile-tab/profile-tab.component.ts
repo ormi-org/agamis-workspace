8a2a47b20b88c4fa9e17fffa7f789923f3dcdfcd
p103304 <ethan.broyard@univ-orleans.Fr>
2023-12-14 17:04:39 +0100

    feat[5]: Modif du sass de l'onglet profile + Ajout du changement d'icone User
    Co écrit avec BriceVolpi

--- apps/workspace-ui/src/app/components/profile-tab/profile-tab.component.ts
+++ apps/workspace-ui/src/app/components/profile-tab/profile-tab.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AvatarService } from '../../services/avatar-service';
 
 @Component({
   selector: 'app-profile-tab',
   templateUrl: './profile-tab.component.html',
   styleUrls: ['./profile-tab.component.sass'],
 })
-export class ProfileTabComponent {}
+export class ProfileTabComponent { 
+  constructor(private avatarService: AvatarService) { }
+
+
+  avatarSrc: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: any): void {
+   const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        this.avatarSrc = reader.result;
+        this.avatarService.changeAvatar(this.avatarSrc);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
   
