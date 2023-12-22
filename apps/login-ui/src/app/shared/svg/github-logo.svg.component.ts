1a293b4a513cc3f79a4b077277229258e4718490
Thibaud Faurie <thibaud.faurie@openrichmedia.org>
2023-12-22 16:42:52 +0000

    feat[23]: login ui
    - adding gitlab github and google svg logo
    - adding msw dependency
    - fixing angular cli version

---
+++ apps/login-ui/src/app/shared/svg/github-logo.svg.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+import { Color } from "../../common/color";
+
+@Component({
+    standalone: true,
+    selector: 'agamis-ws-login-svg-github-logo',
+    templateUrl: "../../../assets/images/svg/github-logo.svg",
+    styles: `
+        :host {
+            display: flex
+        }
+    `,
+})
+export class GithubLogoSvgComponent {
+    @Input()
+    fillColor: string = Color.PRIMARY_ONE;
+}
---
+++ apps/login-ui/src/app/shared/svg/gitlab-logo.svg.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+import { Color } from "../../common/color";
+
+@Component({
+    standalone: true,
+    selector: 'agamis-ws-login-svg-gitlab-logo',
+    templateUrl: "../../../assets/images/svg/gitlab-logo.svg",
+    styles: `
+        :host {
+            display: flex
+        }
+    `,
+})
+export class GitlabLogoSvgComponent {
+    @Input()
+    fillColor: string = Color.PRIMARY_ONE;
+}
