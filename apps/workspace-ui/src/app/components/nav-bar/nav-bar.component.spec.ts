c745d45215b62331c7ea228dfb69764a0957541b
p103304 <ethan.broyard@univ-orleans.Fr>
2024-01-03 21:42:09 +0100

    chore: update ci

--- apps/workspace-ui/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ apps/workspace-ui/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavBarComponent } from './nav-bar.component';
 import { AppModule } from '../../app.module'; 
+import { UserInfoService } from '../../services/user-info-service';
+import { of } from 'rxjs';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async () => {
+    const userInfoServiceMock = {
+      userInfo$: of({
+        identifier:'Chauncey',
+        orgName:"Tyria's heroes",
+        email:'chauncey.vonsnuffles@divinitysreach.tyria',
+        projectsNbr:'8', 
+        avatarSrc:'',
+        timeZone:'America/New_York',
+        Language:'en',
+        Fullname:'@chauncey.vonsnuffles',
+        JobTitle:'Developers'
+      })
+    };
     await TestBed.configureTestingModule({
-      imports: [AppModule] 
+      imports: [AppModule],
+      declarations: [ NavBarComponent ],
+      providers: [
+        { provide: UserInfoService, useValue: userInfoServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavBarComponent);
