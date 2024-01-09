4dabc90118e8f82e596656bb06c52b6a2c743eaa
p103304 <ethan.broyard@univ-orleans.Fr>
2024-01-09 14:34:26 +0100

    chore: e2e in english

--- apps/workspace-ui-e2e/src/e2e/nav-bar.cy.ts
+++ apps/workspace-ui-e2e/src/e2e/nav-bar.cy.ts
@@ -1,22 +1,22 @@
-describe('Test de la Page d\'Accueil', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:4200/index.html'); // chargement de la page d'accueil pour chaque test
-        cy.url().should('include', '/'); // vérification de l'URL pour chaque test
-      });
-  
-      it('Verification que la nav-bar se genere bien', () => {
-        cy.get('.navbar-left h2').contains('agamis');
-      });
-      
-      it('Verification que le menu deroulant du user apparait bien et contient le bouton profile', () => {
-        cy.get('.dropdown-toggle').click(); // Clique sur le bouton pour ouvrir le menu déroulant
-        cy.get('.dropdown-menu').should('be.visible').and('contain', 'Profile');
-      });
-      
-      it('Verification que lorsque l\'on clique sur profile, l\'onglet se genere au centre de la page', () => {
-        cy.get('.dropdown-toggle').click(); // Ouvre le menu déroulant
-        cy.get('.dropdown-menu button').contains('Profile').click(); // Clique sur "Profile"
-        cy.get('.custom-tabs').find('button').contains('Profile').should('be.visible'); // Vérifie que l'onglet "Profile" est présent
-      });
+describe('Home Page Tests', () => {
+  beforeEach(() => {
+      cy.visit('http://localhost:4200/index.html'); // Load the home page for each test
+      cy.url().should('include', '/'); // Verify the URL for each test
   });
+
+  it('Verifies that the nav-bar is properly generated', () => {
+      cy.get('.navbar-left h2').contains('agamis');
+  });
+
+  it('Verifies that the user dropdown menu appears correctly and contains the profile button', () => {
+      cy.get('.dropdown-toggle').click(); // Clicks on the button to open the dropdown menu
+      cy.get('.dropdown-menu').should('be.visible').and('contain', 'Profile');
+  });
+
+  it('Verifies that clicking on profile generates the tab in the center of the page', () => {
+      cy.get('.dropdown-toggle').click(); // Opens the dropdown menu
+      cy.get('.dropdown-menu button').contains('Profile').click(); // Clicks on "Profile"
+      cy.get('.custom-tabs').find('button').contains('Profile').should('be.visible'); // Verifies that the "Profile" tab is present
+  });
+});
   
