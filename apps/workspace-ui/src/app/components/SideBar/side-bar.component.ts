3b157fdf421dfe0f205eea4890d0125fdd64631e
brice.morel <brice.morel1@etu.univ-orleans.fr>
2024-01-11 15:21:33 +0100

    feat[11|12]: Conversion en Ts du composant

--- apps/workspace-ui/src/app/components/SideBar/side-bar.component.ts
+++ apps/workspace-ui/src/app/components/SideBar/side-bar.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-side-bar',
+  selector: 'workspace-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.sass'],
 })
-export class SideBarComponent {}
+export class SideBarComponent {
+// Variables to control the visibility of the div elements
+projectsVisible = false;
+collabVisible = false;
+
+// Function to toggle the visibility of the projects div
+toggleProjects() {
+  this.projectsVisible = !this.projectsVisible;
+  this.collabVisible = false; // Hide collab div when showing projects div
+}
+
+// Function to toggle the visibility of the collab div
+toggleCollab() {
+  this.collabVisible = !this.collabVisible;
+  this.projectsVisible = false; // Hide projects div when showing collab div
+}
+}
+
+
+
 
 
