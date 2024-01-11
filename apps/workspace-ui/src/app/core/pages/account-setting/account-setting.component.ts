61b4b19153ba062fa086a5c63e8a20d58933b961
p103304 <ethan.broyard@univ-orleans.Fr>
2024-01-11 15:38:28 +0100

    feat[7]: generate component

---
+++ apps/workspace-ui/src/app/core/pages/account-setting/account-setting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'agamis-ws-account-setting',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account-setting.component.html',
+  styleUrl: './account-setting.component.scss',
+})
+export class AccountSettingComponent {}
