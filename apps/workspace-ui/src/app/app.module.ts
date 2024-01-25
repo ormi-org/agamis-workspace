62ea876cba77727c503f083a095a9ffdefb979b5
brice.morel <brice.morel1@etu.univ-orleans.fr>
2024-01-25 08:36:15 +0100

    feat[11|12]: Conversion des fichier sass en scss

--- apps/workspace-ui/src/app/app.module.ts
+++ apps/workspace-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { SideBarComponent } from './components/SideBar/side-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, SideBarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
